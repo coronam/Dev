d199c0c325a18b02170dad43a64ec50fbd30607a
coronam <coronam@allegheny.edu>
2014-07-01 13:05:17 -0400

    chang

--- simple/src/HelloWorldMain.java
+++ simple/src/HelloWorldMain.java
@@ -5,6 +5,7 @@ public class HelloWorldMain{
 	HelloWorld2 h2 = new HelloWorld2();
 	HelloWorld3 h3 = new HelloWorld3();
 	HelloWorld4 h4 = new HelloWorld4();
+	System.out.println();
 	System.out.println(h1.toString());
 	System.out.println(h2.toString());
 	System.out.println(h3.toString());
